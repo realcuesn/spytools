d7a03765f67c14bc5d735e2cf3238c7c2562463d
realcuesn <129620236+realcuesn@users.noreply.github.com>
2024-01-06 17:17:08 +0530

    feat:username

--- pages/register.vue
+++ pages/register.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="w-full lg:min-h-screen pt-44 lg:py-20 flex items-center justify-center px-5 xl:px-6"
-  >
+  <div class="w-full lg:min-h-screen pt-44 lg:py-20 flex items-center justify-center px-5 xl:px-6">
     <div class="grid mx-auto items-center lg:grid-cols-2 max-w-7xl w-full">
       <div class="w-full hidden lg:block">
         <img src="@/assets/branding/logo.svg" class="w-[75%]" alt="" />
@@ -11,45 +9,37 @@
           Crear una cuenta
         </div>
         <div class="mt-10 w-full space-y-2">
-          <label class="block text-base uppercase" for="email">Tu correo</label>
-          <input
-            v-model="email"
-            type="email"
+          <label class="block text-base uppercase" for="username">Tu nombre de usuario</label>
+          <input v-model="username" type="text"
             class="text-[#f7ffdd] bg-[#272727] outline-none py-3 pl-5 rounded-md w-full lowercase text-xl placeholder:text-[#A3A3A3]"
-            placeholder=""
-          />
+            placeholder="" />
+        </div>
+        <div class="mt-10 w-full space-y-2">
+          <label class="block text-base uppercase" for="email">Tu correo</label>
+          <input v-model="email" type="email"
+            class="text-[#f7ffdd] bg-[#272727] outline-none py-3 pl-5 rounded-md w-full lowercase text-xl placeholder:text-[#A3A3A3]"
+            placeholder="" />
         </div>
         <div class="mt-5 w-full space-y-2">
-          <label class="block text-base uppercase" for="password"
-            >Tu contraseña</label
-          >
-          <input
-            v-model="password"
-            type="password"
+          <label class="block text-base uppercase" for="password">Tu contraseña</label>
+          <input v-model="password" type="password"
             class="text-[#f7ffdd] bg-[#272727] outline-none py-3 pl-5 rounded-md w-full lowercase text-xl placeholder:text-[#A3A3A3]"
-            placeholder=""
-          />
+            placeholder="" />
         </div>
         <!--                 already have an account -->
         <div class="mt-4 text-lg mx-auto w-fit">
           ¿Ya tienes una cuenta?
-          <NuxtLink to="/login" class="text-[#F7FFDD] font-medium"
-            >Inicia sesión</NuxtLink
-          >
+          <NuxtLink to="/login" class="text-[#F7FFDD] font-medium">Inicia sesión</NuxtLink>
         </div>
 
         <div class="flex flex-col items-center mt-10 w-full gap-y-4">
-          <button
-            @click="register"
-            class="flex items-center py-2 pl-4 text-black pr-5 bg-gradient-to-r gap-x-1 from-[#595CFF] button-gradient-animation rounded-md to-[#F7FFDD]"
-          >
+          <button @click="register"
+            class="flex items-center py-2 pl-4 text-black pr-5 bg-gradient-to-r gap-x-1 from-[#595CFF] button-gradient-animation rounded-md to-[#F7FFDD]">
             <span class="font-semibold">Registrarse</span>
           </button>
 
-          <button
-            @click="registerGoogle"
-            class="flex items-center w-fit py-2 pl-4 text-black pr-5 bg-gradient-to-r gap-x-1 from-[#595CFF] button-gradient-animation rounded-md to-[#F7FFDD]"
-          >
+          <button @click="registerGoogle"
+            class="flex items-center w-fit py-2 pl-4 text-black pr-5 bg-gradient-to-r gap-x-1 from-[#595CFF] button-gradient-animation rounded-md to-[#F7FFDD]">
             <span class="font-semibold">Registrarse con</span>
             <img src="@/assets/icons/google-colored.svg" alt="" />
           </button>
@@ -61,19 +51,26 @@
 
 <script setup>
 const email = ref("");
+const username = ref("");
 const password = ref("");
 const client = useSupabaseClient();
+const router = useRouter();
 const register = async () => {
   let { data, error } = await client.auth.signUp({
     email: email.value,
     password: password.value,
+    options: {
+      data: {
+        username: username.value
+      }
+    }
   });
 
   if (error) {
     console.log(error);
   } else {
     console.log(data);
-    routerKey.push("/login");
+    router.push("/login");
   }
 };
 const registerGoogle = async () => {
