18d35bc36c08843f173eaec66d1d5edb878af750
realcuesn <129620236+realcuesn@users.noreply.github.com>
2024-01-09 01:13:53 +0530

    feat;Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['shadcn-nuxt', '@nuxtjs/supabase', 'nuxt-simple-sitemap', 'nuxt-simple-robots'],
-  css: ['~/assets/css/tailwind.css', '~/assets/fonts/fonts.css'],
+  modules: [
+    "shadcn-nuxt",
+    "@nuxtjs/supabase",
+    "nuxt-simple-sitemap",
+    "nuxt-simple-robots",
+    "nuxt-gtag",
+  ],
+  css: ["~/assets/css/tailwind.css", "~/assets/fonts/fonts.css"],
   shadcn: {
     /**
      * Prefix for all the imported component
      */
-    prefix: '',
+    prefix: "",
     /**
      * Directory that the component lives in.
      * @default "./components/ui"
      */
-    componentDir: './components/ui'
+    componentDir: "./components/ui",
   },
   ssr: true,
   app: {
-    pageTransition: false
+    pageTransition: false,
+  },
+  gtag: {
+    id: "G-C1FVBBDD20",
   },
   supabase: {
     redirect: false,
-    url: 'https://zzjfupocbypxhqvlygyf.supabase.co',
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6amZ1cG9jYnlweGhxdmx5Z3lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3NTA3MDgsImV4cCI6MjAxNjMyNjcwOH0.JL5IIDVLVp8aWWaVqZCBY1ycOztMEh2guLk2aCDPzbc',
+    url: "https://zzjfupocbypxhqvlygyf.supabase.co",
+    key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6amZ1cG9jYnlweGhxdmx5Z3lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3NTA3MDgsImV4cCI6MjAxNjMyNjcwOH0.JL5IIDVLVp8aWWaVqZCBY1ycOztMEh2guLk2aCDPzbc",
   },
   postcss: {
     plugins: {
@@ -29,4 +38,4 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-})
+});
